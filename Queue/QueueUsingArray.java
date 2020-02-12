48db7678d01309075f51077b8576c79370730ee3
AnmolSehgal <45272533+AnmolSehgal@users.noreply.github.com>
2020-02-13 00:15:25 +0530

    Queue Using Array is done

--- Queue/QueueUsingArray.java
+++ Queue/QueueUsingArray.java
@@ -5,13 +5,60 @@ class QueueUsingArray
 {
 	public static void main(String[] args) 
 	{
-		
+		Scanner sc = new Scanner(System.in);
+		Queue q = new Queue(sc.nextInt());
+		while(q.isFull()!=true)
+			q.enQueue(sc.nextInt());
+		if(q.isFull()==true)
+		{
+			System.out.println("Queue is full");
+		}
+		while(q.isEmpty()!=true)
+		{
+			System.out.println(q.deQueue());
+		}
+		if(q.deQueue()==-1)
+		{
+			System.out.println("Queue is empty");
+		}
+
 	}
 }
 
 class Queue
 {
+	int []queueArray;
+	int front,rare;
+	public Queue(int size)
+	{
+		queueArray = new int [size];
+		front  = -1;
+		rare = -1;
+	}
+	public void enQueue(int data)
+	{
+		if(isFull()!=true)
+		{
+			queueArray[++rare] = data;
+		}
+	}
+	public int deQueue()
+	{
+		if(isEmpty()!=true)
+		{
+			return queueArray[++front];
+		}
+		return -1;
+	}
+	public boolean isEmpty()
+	{
+			return (front==rare);
+	}
 	
-	
-	
+	public boolean isFull()
+	{
+			return (rare == queueArray.length-1);
+	}
+
+
 }
