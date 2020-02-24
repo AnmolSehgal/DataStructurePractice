d7c9c86fe79364e2c10b8ea3a346f7f89d273c72
AnmolSehgal <45272533+AnmolSehgal@users.noreply.github.com>
2020-02-24 14:07:45 +0530

    resolved a mistake

--- InsertionSort.java
+++ InsertionSort.java
@@ -8,19 +8,20 @@ class InsertionSort
 		Scanner Sc = new Scanner(System.in);
 		int size = Sc.nextInt();
 		int a[]  = new int[size];
+		int temp;
 		for(int i=0;i<size;i++)
 		{
 			a[i] = Sc.nextInt();
 		}
 		for(int i =0;i<size;i++)
 		{
-			for(int j=i+i;j<size-1;j++)
+			for(int j=i+1;j<size;j++)
 			{
 				if(a[i]>a[j])
 				{
-					a[i]=a[i]+a[j];
-					a[j]=a[i]-a[j];
-					a[i]= a[i]-a[j];
+					temp=a[i];
+					a[i]=a[j];
+					a[j]= temp;
 				}
 			}
 		}
